b1159563d5dc7d636db5ac9235af200f093033d2
Kunal719 <kunalsrivastav719@gmail.com>
2024-05-06 16:56:59 +0530

    implement sidebar and layout

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,8 +1,36 @@
+import HeaderBox from '@/components/HeaderBox'
+import RightSideBar from '@/components/RightSideBar'
+import TotalBalanceBox from '@/components/TotalBalanceBox'
 import React from 'react'
 
 const Home = () => {
+    const loggedIn = { firstName: 'Kunal', lastName: 'Srivastav', email: 'kunal@gmail.com' }
     return (
-        <div>Home</div>
+        <section className='home'>
+            <div className='home-content'>
+                <header className='home-header'>
+                    <HeaderBox
+                        type='greeting'
+                        title='Welcome,'
+                        subtext='Access and manage your account and transactions efficiently'
+                        user={loggedIn?.firstName || 'Guest'}
+                    />
+                    <TotalBalanceBox
+                        accounts={[]}
+                        totalBanks={1}
+                        totalCurrentBalance={1250.35}
+                    />
+                </header>
+
+                RECENT TRANSACTIONS
+            </div>
+
+            <RightSideBar
+                user={loggedIn}
+                transcations={[]}
+                banks={[{ currentBalance: 1250.35 }, { currentBalance: 1300.41 }]}
+            />
+        </section>
     )
 }
 
